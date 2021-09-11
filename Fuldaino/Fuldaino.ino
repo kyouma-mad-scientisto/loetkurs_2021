3c89f96ff6684b6c772edd63e6d71a8490c12d8a
Yves Ehrlich <yves.ehrlich@informatik.hs-fulda.de>
2021-09-11 13:21:55 +0200

    Initial commit

---
+++ Fuldaino/Fuldaino.ino
@@ -0,0 +1,19 @@
+/*
+ * Code fuer Badget zum Loetkurs an der HS-Fulda am Fachbereich AI
+ * 
+ * 8x8 LED Matrix
+ * Atmega328p mit internem 8MHz Takt
+ * 
+ * 
+ */
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
